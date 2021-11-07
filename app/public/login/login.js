e0c79517f78510192ce964f6620689559603f1b1
AmigoDroid <colteman05@gmail.com>
2021-11-07 00:10:13 -0200

    login

--- app/public/login/login.js
+++ app/public/login/login.js
@@ -2,14 +2,12 @@ const forme = document.getElementById('formulario-login');
 const adminUser = 'adminadmin'
 const adminPass = 'adminadmin'
 const UrlServer_DB = 'http://api-server-delivery.herokuapp.com/';
-
+const laod = document.getElementById('load');
 
 forme.addEventListener('submit', function (e) {
     e.preventDefault();
-
     const username = document.getElementById('tel').value;
     const password = document.getElementById('pas').value;
-
 
     if (username.length <= 0 || password.length <= 0) {
         console.log('vazio');
@@ -18,13 +16,14 @@ forme.addEventListener('submit', function (e) {
 
     } else {
         login(username, password);
+      //  laod.style.visibility='visible'
     }
 
 });
 //fazer o login
 
  function login(tel, pass) {
-
+    
     const body = {
         telefone: tel,
         password: pass
@@ -34,6 +33,7 @@ forme.addEventListener('submit', function (e) {
   
  }
  function verificar(retorno){
+  ///  laod.style.visibility='visibility=hidden'
     console.log("retorno: "+retorno);
     const dados = JSON.parse(retorno);
     let status = dados.status;
@@ -42,7 +42,39 @@ forme.addEventListener('submit', function (e) {
         alert("logado com sucesso! seu token: "+dados.token);
         localStorage.setItem('token',dados.token);
     } else {
+       
         alert('telefone ou senha incorretos')
         console.log(dados.status);
+        
     }
+ }
+ function renderLogin(){
+document.getElementById('login-tela').innerHTML=` <h1>Login</h1>
+<form id="formulario-login">
+    <div class="div-input">
+        <input type="text" id="tel"  required>
+        <label>Telefone</label>
+    </div>
+    <div class="div-input">
+        <input type="text" id="pas"  required>
+        <label>Senha</label>
+    </div>
+    <button>Login</button>
+    <p>Não é cadastrado? <a href="#">Cadastrar</a> <br> Esqueceu a senha? <a href="#">Recuperar</a></p>
+</form>`
+ }
+ function renderload(){
+     document.getElementById('login-tela').innerHTML=` <h1>Login</h1>
+     <form id="formulario-login">
+         <div class="div-input">
+             <input type="text" id="tel"  required>
+             <label>Telefone</label>
+         </div>
+         <div class="div-input">
+             <input type="text" id="pas"  required>
+             <label>Senha</label>
+         </div>
+         <img id='imagem' src="../../src/img/loading.gif" alt="">
+         <p>Não é cadastrado? <a href="#">Cadastrar</a> <br> Esqueceu a senha? <a href="#">Recuperar</a></p>
+     </form>`
  }
