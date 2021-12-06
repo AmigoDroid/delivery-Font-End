6a4453c01157473011d0a58dc213dc802488f8f3
AmigoDroid <colteman05@gmail.com>
2021-12-05 22:04:53 -0200

    auto-login

--- app/public/splash/index.js
+++ app/public/splash/index.js
@@ -4,3 +4,20 @@ function login(){
 function cadastrar(){
     location.assign('../cadastro/cadastrar.html');
 }
+window.onload =  function(){
+    console.log('onload');
+    const url="https://api-server-delivery.herokuapp.com/home/cliente/";
+    const token=localStorage.getItem('token');
+    const logar = get(url+token);
+    const dados = JSON.parse(logar);
+    if(dados.status){ 
+        console.log(dados.status);
+            const res =  confirm('deseja fazer login?');
+            if(res){
+                //logar
+                location.assign('../home/index.html');
+            }else{
+                //não
+            }
+    }
+}
