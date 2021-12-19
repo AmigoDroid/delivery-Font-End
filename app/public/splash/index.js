5af4de1ec726f7e7b7f36790cf5522974a5ec2b5
AmigoDroid <colteman05@gmail.com>
2021-12-18 23:19:13 -0200

    nova tela

--- app/public/splash/index.js
+++ app/public/splash/index.js
@@ -4,11 +4,41 @@ function login(){
 function cadastrar(){
     location.assign('../cadastro/cadastrar.html');
 }
+
+function RenderCard(dados){
+    let {nome,descrision,telefone}=dados;
+    document.getElementById('conteudo').innerHTML+=`
+    <div class="card">
+    <div class="img">
+        <img src="../../src/img/lago-verde-img.png" alt="card-img">
+    </div>
+    <div class="titleCard">
+        <h1>${nome}</h1>
+    </div>
+    <div class="descrision">
+        <p>${descrision} <br> Telefone:${telefone}</p>
+    </div>
+    <div class="button" onclick='whatsapp(${telefone})'>
+        <img src="../../src/img/whatsapp.png" alt="icon-whatsaap">
+        <p>contato</p>
+    </div>
+</div>
+    `;
+}
+function whatsapp(tel){
+    alert('telefone: '+tel);
+}
+
+
+
+
+
+
 window.onload =  function(){
     console.log('onload');
     const url="https://api-server-delivery.herokuapp.com/home/cliente/";
     const token=localStorage.getItem('token');
-    const logar = get(url+token);
+  //  const logar = get(url+token);
     const dados = JSON.parse(logar);
     if(dados.status){ 
         console.log(dados.status);
