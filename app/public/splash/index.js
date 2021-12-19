4dc76abcab4c4b9f7a8b833ff268794a4dcd9e2f
AmigoDroid <colteman05@gmail.com>
2021-12-19 00:16:48 -0200

    mensagem

--- app/public/splash/index.js
+++ app/public/splash/index.js
@@ -6,6 +6,7 @@ function cadastrar(){
 }
 
 function RenderCard(dados){
+    console.log('Render Card');
     let {nome,descrision,telefone}=dados;
     document.getElementById('conteudo').innerHTML+=`
     <div class="card">
@@ -30,12 +31,51 @@ function whatsapp(tel){
 }
 
 
-
-
+function render(){
+    const url='https://api-server-delivery.herokuapp.com/splash/delivery';
+    const lista = get(url,'');
+    const dados = JSON.parse(lista);
+    console.log(dados);
+    if(dados.status===true){
+        console.log(dados.body.length -1);
+       
+        for(let i =0; i < dados.body.length;i++){
+            let body =dados.body;
+            console.log(body[0]);
+            RenderCard(body[i]);
+        }
+    }else{
+        console.log('vazio');
+    }
+    
+}
+function msg(texto){
+    document.getElementById('textMSG').innerText=texto;
+}
+function mensagem(){
+    const data = new Date;
+    const hora = data.getHours();
+   console.log(hora);
+    if(hora>=5 && hora <12){
+        //bom dia
+        msg('Olá, Bom Dia!');
+    }else if(hora>=12 && hora <18){
+        //boa tarde
+        msg('Olá, Boa Tarde!')
+    }else if(hora>=18 && hora <24){
+        //boa noite
+        msg('Olá, Boa Noite!')
+    }else{
+        //boa madrugada
+        msg('Boa Madrogada!')
+    }
+}
 
 
 window.onload =  function(){
     console.log('onload');
+    render();
+    mensagem();
     const url="https://api-server-delivery.herokuapp.com/home/cliente/";
     const token=localStorage.getItem('token');
   //  const logar = get(url+token);
