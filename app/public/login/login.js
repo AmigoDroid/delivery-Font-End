b7c53fe52b617507e9d38b4ca594b27e72914473
AmigoDroid <canalmeuovo36@gmail.com>
2022-03-02 23:55:40 -0300

    update login via web socket

--- app/public/login/login.js
+++ app/public/login/login.js
@@ -1,48 +1,48 @@
-const forme = document.getElementById('formulario-login');
-const adminUser = 'adminadmin'
-const adminPass = 'adminadmin'
-const UrlServer_DB = 'https://api-server-delivery.herokuapp.com/';
-const laod = document.getElementById('load');
+// const forme = document.getElementById('formulario-login');
+// const adminUser = 'adminadmin'
+// const adminPass = 'adminadmin'
+// const UrlServer_DB = 'https://api-server-delivery.herokuapp.com/';
+// const laod = document.getElementById('load');
 
-forme.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const username = document.getElementById('tel').value;
-    const password = document.getElementById('pas').value;
+// forme.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     const username = document.getElementById('tel').value;
+//     const password = document.getElementById('pas').value;
 
-    if (username.length <= 0 || password.length <= 0) {
-        console.log('vazio');
-    } else if (username == adminUser && password == adminPass) {
-        location.replace('../../painel/loginAdmin/login.html');
+//     if (username.length <= 0 || password.length <= 0) {
+//         console.log('vazio');
+//     } else if (username == adminUser && password == adminPass) {
+//         location.replace('../../painel/loginAdmin/login.html');
 
-    } else {
-        login(username, password);
-      //  laod.style.visibility='visible'
-    }
+//     } else {
+//         login(username, password);
+//       //  laod.style.visibility='visible'
+//     }
 
-});
-//fazer o login
+// });
+// //fazer o login
 
- function login(tel, pass) {
+//  function login(tel, pass) {
     
-    const body = {
-        telefone: tel,
-        password: pass
-    };
+//     const body = {
+//         telefone: tel,
+//         password: pass
+//     };
 
-    const retorno = postLogin(UrlServer_DB + "loginUser", body);
+//     const retorno = postLogin(UrlServer_DB + "loginUser", body);
   
- }
- function verificar(retorno){
-  ///  laod.style.visibility='visibility=hidden'
-    console.log("retorno: "+retorno);
-    const dados = JSON.parse(retorno);
-    let status = dados.status;
-    if (dados.status == true) {
-        localStorage.setItem('token',dados.token);
-        location.replace('../home/index.html');
-    } else {
-        alert('telefone ou senha incorretos')
-        console.log(dados.status);
+//  }
+//  function verificar(retorno){
+//   ///  laod.style.visibility='visibility=hidden'
+//     console.log("retorno: "+retorno);
+//     const dados = JSON.parse(retorno);
+//     let status = dados.status;
+//     if (dados.status == true) {
+//         localStorage.setItem('token',dados.token);
+//         location.replace('../home/index.html');
+//     } else {
+//         alert('telefone ou senha incorretos')
+//         console.log(dados.status);
         
-    }
- }
+//     }
+//  }
